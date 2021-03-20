106518a2fb8a4b90653c4c258c36dbcdf280cc16
Rutger van Bergen <rbergen@xs4all.nl>
2021-03-20 12:51:08 +0100

    Fixed small bug in word wrapping

--- include/templates/console.h
+++ include/templates/console.h
@@ -34,7 +34,8 @@ void Window::check_line_ends(const std::basic_string<TChar>& text, TChar c)
 	if (text.empty())
 		return;
 
-	if (text.back() != c)
+	// line break can be explicit (through c), or implicit because the text on the last line of output is as wide as the window
+	if (text.back() != c && (text.length() + get_x()) % getmaxx(wnd))
 	{
 		clear_line_end();
 		return;
@@ -75,30 +76,43 @@ void Window::print_template(const std::basic_string<TChar>& text, TChar space, T
 	int width = getmaxx(wnd);
 	size_t break_index;
 
-	while (copy.length() > (width - get_x()))
+	// we may not be at the start of the line
+	size_t remaining_width = (size_t)width - get_x();
+	while (copy.length() > remaining_width)
 	{
+		// find the first line break within remaining width, or the last space if there is none
 		break_index = copy.find(line_break);
-
-		if (break_index == string::npos || break_index > (width - get_x()))
-			break_index = copy.find_last_of(space, width - get_x());
+		if (break_index == string::npos || break_index > remaining_width)
+			break_index = copy.find_last_of(space, remaining_width);
 	
+		// check if we have an uninterrupted character sequence that surpasses remaining width
 		if (break_index == string::npos) 
 		{
+			// if so, try again on a new line or make a hard cut at window width if this already is one
 			if (get_x() == 0)
 				break_index = width;
 			else
 			{
 				print(line_break);
+				remaining_width = width;
 				continue;
 			}
 		}
 
+		// print the line we found (terminated by a line break, a space, or a hard cut), adding a line break if necessary 
 		print_line(copy.substr(0, break_index));
-
 		if (break_index != width)
 			print(line_break);
 
-		copy.erase(0, break_index + 1);
+		remaining_width = width;
+
+		copy.erase(0, break_index);
+
+		// remove the break character, unless we made a hard cut in an uninterrupted character sequence
+		if (copy.front() == space || copy.front() == line_break)
+			copy.erase(0, 1);
+
+		// remove leading spaces in the remaining text
 		while (!copy.empty() && copy.front() == space)
 			copy.erase(0, 1);
 	} 
